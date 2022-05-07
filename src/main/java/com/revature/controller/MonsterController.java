16feefeb9f460deeb444f3a6b6612bf48a6d5bea
Erick C <xuerick12@gmail.com>
2022-05-06 23:32:58 -0700

    added some comments

--- src/main/java/com/revature/controller/MonsterController.java
+++ src/main/java/com/revature/controller/MonsterController.java
@@ -19,7 +19,7 @@ import com.revature.entities.Monster;
 import com.revature.service.MonsterService;
 
 @RestController("monsterController")
-@RequestMapping("/monster")
+@RequestMapping("/monster")////http://localhost/monster
 public class MonsterController
 {
 	private MonsterService monsterService;
@@ -31,7 +31,8 @@ public class MonsterController
 		this.monsterService = monsterService;
 	}
 	
-	
+	//GET request
+	//http://localhost/monster/all
 	@GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Monster>> findAll()
 	{
@@ -39,17 +40,19 @@ public class MonsterController
 		return httpResponse;
 	}
 	
-	
+	//POST request
+	//http://localhost/monster/new -> Insert into body in POstman App
 	@PostMapping(value = "/new", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void save(@RequestBody Monster monster)
 	{
-		//this finally works. we ave to insert the JSON into the body using post
+		//this finally works. we have to insert the JSON into the body using post
 		this.monsterService.save(monster);
 //		ResponseEntity<Monster> response = restTemplate.postForEntity("http://localhost:8080/monster", monster, Monster.class);
 //		return response.getBody();
 	}
 	
-	
+	//GET request
+	//http://localhost/monster/"monsterName" -> to find a monster in the database we've created
 	@GetMapping(value = "/{name}", produces =MediaType.APPLICATION_JSON_VALUE)
 	public Monster findMonsterByName(@PathVariable("name") String name)
 	{
@@ -57,6 +60,9 @@ public class MonsterController
 		return this.monsterService.findMonsterbyName(name); 
 	}
 	
+	
+	//DELETE request
+	//http://localhost/monster/"monsterName" -> to delete a monster in the database by name
 	@DeleteMapping(value = "/{name}", produces =MediaType.APPLICATION_JSON_VALUE)
 	public void delete(@PathVariable("name") Monster monster)
 	{
